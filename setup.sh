3ccba774dd97230eeb591c0d69e08673f3e2dc46
sunnycodes3114 <g11ascsannidhay.tws@gmail.com>
2024-11-28 14:47:06 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,11 @@
 # Upgrade pip
 pip install --upgrade pip
 
-# Install PyTorch and dependencies
+# Install PyTorch (CPU version) and TorchVision
 pip install torch==2.0.1+cpu torchvision==0.15.2+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
-# Install detectron2 from GitHub
+# Install Detectron2
 pip install git+https://github.com/facebookresearch/detectron2.git
+
+# Install other required libraries
+pip install -r requirements.txt
